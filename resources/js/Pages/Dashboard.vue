45c844061a3eed4c827c10b0afd8ff8aff2ea107
Ale <cueva_skate@outlook.es>
2022-10-18 11:14:55 -0500

    first commit

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -7,8 +7,14 @@ import Welcome from '@/Components/Welcome.vue';
     <AppLayout title="Dashboard">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
+                
             </h2>
+            <select class="form-select" aria-label="Default select example">
+  <option selected>Open this select menu</option>
+  <option value="1">One</option>
+  <option value="2">Two</option>
+  <option value="3">Three</option>
+</select>
         </template>
 
         <div class="py-12">
